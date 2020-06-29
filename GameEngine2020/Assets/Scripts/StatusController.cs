d1b9089a00e4c230f8e4e2824619239cb53f4f07
KimDongSoon <eutteumiyo@naver.com>
2020-06-29 18:30:49 +0900

    Gas UI
    연료 UI를 배치하고
    StatusController를 만들어서 시간에 따라 연료가 소모되는 것을 구현

---
+++ GameEngine2020/Assets/Scripts/StatusController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusController : MonoBehaviour
+{
+    [SerializeField]
+    private int gas;        // 연료의 최대 양
+    private int currentGas; // 현재 연료의 양
+
+    // 연료 증가량
+    [SerializeField]
+    private int gasIncreaseSpeed;   // 연료 증가량
+
+    // 연료 회복 딜레이
+    private int gasRechargeTime;
+    private int currentGasRechargeTime;
+
+    // 연료가 줄어드는 속도
+    [SerializeField]
+    private int gasDecreaseTime;
+    private int currentGasDecreaseTime;
+
+    // 연료 감소 여부
+    private bool gasUsed;
+
+    // 필요한 이미지
+    [SerializeField]
+    private Image image_Gas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentGas = gas;       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Gas();
+        //DecreaseGas();
+        GasUpdate();
+    }
+
+    private void Gas()              // 연료가 닳는 함수
+    {
+        if(currentGas > 0)
+        {
+            if (currentGasDecreaseTime <= gasDecreaseTime)
+                currentGasDecreaseTime++;
+            else
+            {
+                currentGas--;
+                currentGasDecreaseTime = 0;
+            }
+        }
+    }
+
+
+    private void DecreaseGas(int _count)
+    {
+        if (currentGas - _count < 0)
+            currentGas = 0;
+        else
+            currentGas = -_count;
+    }
+
+    private void GasUpdate()
+    {
+        image_Gas.fillAmount = (float)currentGas / gas;
+    }
+
+}
